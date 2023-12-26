d59cc022e3aa21d4f8b5420703d146bb217c614c
Mui <m.samita.tt@gmail.com>
2023-12-27 05:24:29 +0700

    feat: completed ex1-2

--- ex-1.js
+++ ex-1.js
@@ -1,9 +1,46 @@
 //Exercise 1: Roman to Integer
+const romanNumbers = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+};
 
 const romanToInt = function (s) {
-  //Start coding here
+  let acc = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "I" && s[i + 1] === "V") {
+      acc += 4;
+      i++;
+    } else if (s[i] === "I" && s[i + 1] === "X") {
+      acc += 9;
+      i++;
+    } else if (s[i] === "X" && s[i + 1] === "L") {
+      acc += 40;
+      i++;
+    } else if (s[i] === "X" && s[i + 1] === "C") {
+      acc += 90;
+      i++;
+    } else if (s[i] === "C" && s[i + 1] === "D") {
+      acc += 400;
+      i++;
+    } else if (s[i] === "C" && s[i + 1] === "M") {
+      acc += 900;
+      i++;
+    } else {
+      acc += romanNumbers[s[i]];
+    }
+  }
+  return acc;
 };
 
 const result1 = romanToInt("III"); // 3
 const result2 = romanToInt("LVIII"); // 58
 const result3 = romanToInt("MCMXCIV"); //1994
+
+console.log(result1);
+console.log(result2);
+console.log(result3);
--- ex-2.js
+++ ex-2.js
@@ -1,7 +1,17 @@
-//Exercise 2: Fizz Buzz
-
 const fizzBuzz = function (n) {
-  //Start coding here
+  let result = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      result.push("FizzBuzz");
+    } else if (i % 5 === 0) {
+      result.push("Buzz");
+    } else if (i % 3 === 0) {
+      result.push("Fizz");
+    } else {
+      result.push(String(i));
+    }
+  }
+  return result;
 };
 
 const result1 = fizzBuzz(3);
@@ -10,4 +20,4 @@ const result3 = fizzBuzz(15);
 
 console.log(result1); // ["1","2","Fizz"]
 console.log(result2); // ["1","2","Fizz","4","Buzz"]
-console.log(result3); // ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+console.log(result3);
